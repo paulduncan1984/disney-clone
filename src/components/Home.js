c468216584ee64c227b920fad9e9b9762c49c680
Paul Duncan <paul.duncan1984@gmail.com>
2022-11-23 16:24:42 +1100

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
+
+// const Home = styled.div``;
