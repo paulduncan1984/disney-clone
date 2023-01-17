086f27c19f56a942793ebc0cc742c8f37023e341
Paul Duncan <paul.duncan1984@gmail.com>
2023-01-17 13:51:54 +1100

    add router dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
+import Detail from "./components/Detail";
 import { Counter } from "./features/counter/Counter";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -6,8 +8,13 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/detail" element={<Detail/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
